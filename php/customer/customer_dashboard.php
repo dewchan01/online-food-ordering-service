c37b067d4ab0eb737f8743b7455c110e11ec9b47
Chan De Wei <deweichan@gmail.com>
2023-09-24 03:04:31 +0800

    add delivery address

--- php/customer/customer_dashboard.php
+++ php/customer/customer_dashboard.php
@@ -9,6 +9,28 @@ if (!isset($_SESSION["username"])) {
 // Fetch customer-specific data from the database
 $customerUsername = $_SESSION["username"];
 // Replace this with actual database queries to fetch customer data
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch the customer's address from the database
+$address = "";
+$sql = "SELECT address FROM users WHERE username = '$customerUsername'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $address = $row["address"];
+}
+
+$conn->close();
 ?>
 
 <!DOCTYPE html>
@@ -30,7 +52,10 @@ $customerUsername = $_SESSION["username"];
         <option value="vendora">Vendor A</option>
     </select> -->
     <div id ="products-list"></div>
-        
+    <div id="address">
+        <h3>Delivery Now:</h3>
+    <p><?php echo $address; ?></p>
+</div>
     <div>
         <h3 id="cart-icon" alt="Cart">Order Details</h3>
         <div id="cart-dropdown" class="cart-dropdown">
