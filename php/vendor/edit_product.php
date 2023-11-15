a072e75412dd3ab82563e3668251381d5d672c5d
Chan De Wei <deweichan@gmail.com>
2023-11-15 16:17:01 +0800

    finalize code

--- php/vendor/edit_product.php
+++ php/vendor/edit_product.php
@@ -91,35 +91,63 @@ $conn->close();
 </head>
 
 <body>
-  <h1>Edit Product</h1>
+  <div class="nav">
+    <div class="header">
+      <a href="vendor_dashboard.php" class="headerLogo"><img src="../../images/logo.png" alt="Domini's Logo" height="21" width="23">Domini's</a>
+    </div>
+    <div class="my-account">
+      <div id="my-account-container" style="cursor: pointer;">
+        <a><svg width="26px" height="24px" viewBox="0 0 38 36" style="padding-top:7px;">
+            <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+              <g id="My-Account" fill="#202020">
+                <path d="M18.82 35.964c9.96 0 18.248-8.042 18.248-17.732S28.758.5 18.798.5C8.862.5.596 8.542.596 18.232s8.288 17.732 18.225 17.732zm0-11.821c-5.17 0-9.168 1.691-11.042 3.669a14.102 14.102 0 0 1-3.704-9.58c0-7.998 6.526-14.392 14.724-14.392 8.22 0 14.792 6.394 14.815 14.392 0 3.691-1.4 7.053-3.704 9.58-1.874-1.978-5.894-3.67-11.088-3.67zm0-2.9c3.41.043 6.098-2.791 6.098-6.505 0-3.493-2.687-6.372-6.097-6.372-3.388 0-6.098 2.879-6.075 6.372.022 3.714 2.665 6.46 6.075 6.504z"></path>
+              </g>
+            </g>
+          </svg></a>
+        <a>MY ACCOUNT</a>
+      </div>
+      <div class="basic-buttons-container" id="my-account-nav">
+        <a href="../edit_profile.php" class="basic-buttons" id="edit-profile"><img src="../../images/acc-3.png" alt="User's Account" width="34" height="30">
+          <p style="margin:5px 0 0 8px;">Edit Profile</p>
+        </a>
+        <a href="../logout.php" class="basic-buttons" id="log-out" onclick="return confirm('Are you sure you want to log out?')"><img src="../../images/vector.svg" alt="Log Out" width="34" height="20" style="margin-top:5px;">
+          <p style="margin:4px 0 0 7px;">Logout</p>
+        </a>
+      </div>
+    </div>
+  </div>
+  <h1 style="text-align:center;">Edit Product</h1>
   <?php if (isset($product)) : ?>
-    <form method="POST" action="edit_product.php?product_id=<?php echo $productId; ?>" enctype="multipart/form-data">
-      <input type="hidden" name="product_id" value="<?php echo $productId; ?>">
-      <label for="edited_product_name">Product Name:</label>
-      <input type="text" id="edited_product_name" name="edited_product_name" value="<?php echo $product['product_name']; ?>" required><br>
+    <div class="login-signup">
+      <a href="vendor_dashboard.php" id="back">Back</a>
+      <div id="user-login" style="text-align:left;">
+        <form method="POST" action="edit_product.php?product_id=<?php echo $productId; ?>" enctype="multipart/form-data">
+          <input type="hidden" name="product_id" value="<?php echo $productId; ?>">
+          <label for="edited_product_name" class="user-input">Product Name:</label>
+          <input type="text" id="edited_product_name" class="user-input" name="edited_product_name" value="<?php echo $product['product_name']; ?>" required><br>
 
-      <label for="edited_product_description">Product Description:</label>
-      <textarea id="edited_product_description" name="edited_product_description" required><?php echo $product['description']; ?></textarea><br>
+          <label for="edited_product_description" class="user-input" style="margin:10px 0 10px 0;">Product Description:</label>
+          <input type="text" id="edited_product_description" class="user-input" name="edited_product_description" required value="<?php echo $product['description']; ?>"><br>
 
-      <label for="edited_product_price">Product Price:</label>
-      <input type="number" id="edited_product_price" name="edited_product_price" step="0.01" value="<?php echo $product['price']; ?>" required><br>
+          <label for="edited_product_price" class="user-input" style="margin:10px 0 10px 0;">Product Price:</label>
+          <input type="number" id="edited_product_price" class="user-input" name="edited_product_price" step="0.01" value="<?php echo $product['price']; ?>" required><br>
 
-      <label for="edited_image_url">Image:</label>
-      <input type="file" id="edited_image_url" name="edited_image_url"><br>
+          <label for="edited_image_url" class="user-input" style="margin-right:20px;">Image:</label>
+          <input type="file" id="edited_image_url" style="margin-bottom:25px;" name="edited_image_url"><br>
 
-      <label for="edited_product_status">Product Status:</label>
-      <select id="edited_product_status" name="edited_product_status" required>
-        <option value="available" <?php if ($product['status'] === 'available') echo 'selected'; ?>>Available</option>
-        <option value="unavailable" <?php if ($product['status'] === 'unavailable') echo 'selected'; ?>>Unavailable</option>
-      </select><br>
+          <label for="edited_product_status" class="user-input">Product Status:</label>
+          <select id="edited_product_status" class="user-input" name="edited_product_status" required>
+            <option value="available" <?php if ($product['status'] === 'available') echo 'selected'; ?>>Available</option>
+            <option value="unavailable" <?php if ($product['status'] === 'unavailable') echo 'selected'; ?>>Unavailable</option>
+          </select><br>
 
-      <input type="submit" name="edit_product_submit" value="Save Changes" onclick="alert('Are you sure you want to make changes?')">
-    </form>
+          <input type="submit" class="insert-order-btn" name="edit_product_submit" value="Save Changes" onclick="return confirm('Are you sure you want to make changes?')">
+        </form>
+      </div>
+    </div>
   <?php else : ?>
     <p>Product not found.</p>
   <?php endif; ?>
-
-  <a href="vendor_dashboard.php">Back to Dashboard</a>
-</body>
+  <script src="../../js/script.js"></script>
 
 </html>
